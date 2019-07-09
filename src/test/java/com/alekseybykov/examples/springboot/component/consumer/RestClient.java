a6569fda839c64a2927d09429b81499ff119a6a9
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-07-09 06:08:06 +0400

    Added 'U'pdate and 'D'elete methods into the CRUD client.

--- src/test/java/com/alekseybykov/examples/springboot/component/consumer/RestClient.java
+++ src/test/java/com/alekseybykov/examples/springboot/component/consumer/RestClient.java
@@ -2,6 +2,7 @@ package com.alekseybykov.examples.springboot.component.consumer;
 
 import com.alekseybykov.examples.springboot.component.consumer.dto.PersonDTO;
 import com.alekseybykov.examples.springboot.component.consumer.util.AuthUtil;
+import com.google.common.base.Preconditions;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,18 +17,19 @@ public class RestClient {
     private final static String contextPath = "http://localhost:8080/application";
 
     public static void main(String args[]) {
-        RestClient restClient = new RestClient();
-        restClient.createPerson();
-        restClient.readPerson();
-//        restClient.updatePerson();
-//        restClient.deletePerson();
+        RestClient client = new RestClient();
+        // CRUD
+        client.createPerson();
+        client.readPerson();
+        client.updatePerson();
+        client.deletePerson();
     }
 
     private void createPerson() {
         RestTemplate restTemplate = new RestTemplate();
         String url = String.format("%s/%s", contextPath, "person/add");
 
-        PersonDTO personDTO = PersonDTO.builder()
+        PersonDTO dto = PersonDTO.builder()
                 .id(1L)
                 .firstName("A")
                 .lastName("B")
@@ -35,49 +37,55 @@ public class RestClient {
 
         ResponseEntity<String> responseEntity = restTemplate.exchange(
                 url, HttpMethod.POST, AuthUtil.createEntityWithBasicAuth(
-                        personDTO, MediaType.ALL, username, password), String.class);
+                        dto, MediaType.ALL, username, password), String.class);
+        Preconditions.checkState(responseEntity.getStatusCode().is2xxSuccessful());
 
         // Should print 'Response 201 CREATED'
         System.out.println(responseEntity.toString());
     }
 
-    private void readPerson() {
+    private PersonDTO readPerson() {
         RestTemplate restTemplate = new RestTemplate();
         String url = String.format("%s/%s", contextPath, "person/get/1");
 
         ResponseEntity<PersonDTO> responseEntity = restTemplate.exchange(
                 url, HttpMethod.GET, AuthUtil.createEntityWithBasicAuth(
                         null, MediaType.ALL, username, password), PersonDTO.class);
+        Preconditions.checkState(responseEntity.getStatusCode().is2xxSuccessful());
 
         // Should print 'Response 200 OK' and person details
         System.out.println(responseEntity.toString());
+
+        return responseEntity.getBody();
     }
 
-//    private void updatePerson() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        String url = "http://localhost:8080/person/update";
-//
-//        Person person = new Person();
-//        person.setId(1L);
-//        person.setFirstName("C");
-//        person.setLastName("D");
-//
-//        HttpEntity<Person> requestEntity = new HttpEntity<>(person, headers);
-//        restTemplate.put(url, requestEntity);
-//    }
-//
-//    private void deletePerson() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        String url = "http://localhost:8080/person/delete/{id}";
-//
-//        HttpEntity<Person> requestEntity = new HttpEntity<>(headers);
-//        restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, Void.class, 1L);
-//
-//    }
+    private void updatePerson() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format("%s/%s", contextPath, "person/update");
+
+        PersonDTO dto = readPerson();
+        dto.setFirstName("C");
+        dto.setLastName("D");
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                url, HttpMethod.PUT, AuthUtil.createEntityWithBasicAuth(
+                        dto, MediaType.ALL, username, password), String.class);
+        Preconditions.checkState(responseEntity.getStatusCode().is2xxSuccessful());
+
+        // Should print 'Response 200 OK' and updated person details
+        System.out.println(responseEntity.toString());
+    }
+
+    private void deletePerson() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format("%s/%s", contextPath, "person/delete/1");
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                url, HttpMethod.DELETE, AuthUtil.createEntityWithBasicAuth(
+                        null, MediaType.ALL, username, password), String.class);
+        Preconditions.checkState(responseEntity.getStatusCode().is2xxSuccessful());
+
+        // Should print 'Response 204 NO_CONTENT'
+        System.out.println(responseEntity.toString());
+    }
 }
